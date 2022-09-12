f8355a093df52adc6b4df5b1ea6d95a69b62faeb
hotshotsreeram <hotshotsreeram@gmail.com>
2022-09-12 15:35:08 -0400

    26th commit

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.project_public_subnet.*.id
+  subnet_id      = aws_subnet.project_public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
